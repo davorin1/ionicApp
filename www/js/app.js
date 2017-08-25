c91deeeff8faf347da8c94081354cbfe18fc09e6
Davorin <davorin.prislin@gmail.com>
2017-08-25 10:16:28 +0200

    App Example

--- www/js/app.js
+++ www/js/app.js
@@ -22,3 +22,99 @@ angular.module('starter', ['ionic'])
     }
   });
 })
+
+.config(function($stateProvider, $urlRouterProvider) {
+  $stateProvider
+      .state('tabs', {
+        url: '/tab',
+        abstract: true,
+        templateUrl: 'templates/tabs.html'
+      })
+
+      .state('tabs.list', {
+        url: '/list',
+        views: {
+          'list-tab' : {
+            templateUrl: 'templates/list.html',
+            controller: 'ListController'
+          }
+        }
+      })
+
+      .state('tabs.home', {
+          url: '/home',
+          views: {
+              'home-tab' : {
+                  templateUrl: 'templates/home.html'
+              }
+          }
+      })
+
+      .state('tabs.detail', {
+          url: '/list/:aId',
+          views: {
+              'list-tab' : {
+                  templateUrl: 'templates/detail.html',
+                  controller: 'ListController'
+              }
+          }
+      })
+
+      .state('tabs.calendar', {
+          url: '/calendar',
+          views: {
+              'calendar-tab' : {
+                  templateUrl: 'templates/calendar.html',
+                  controller: 'CalendarController'
+              }
+          }
+      })
+
+      $urlRouterProvider.otherwise('/tab/home');
+})
+
+
+
+.controller('ListController', ['$scope', '$http', '$state', function($scope, $http, $state) {
+    $http.get('js/data.json').success(function(data) {
+      $scope.artists = data.artists;
+      $scope.whichartist = $state.params.aId;
+      $scope.data = { showDelete: false, showReorder: false}
+
+      $scope.moveItem = function(item, fromIndex, toIndex) {
+        $scope.artists.splice(fromIndex, 1);
+        $scope.artists.splice(toIndex, 0, item);
+      };
+      $scope.onItemDelete = function(item) {
+        $scope.artists.splice($scope.artists.indexOf(item), 1);
+      };
+      $scope.toggleStar = function(item) {
+        item.star = !item.star;
+      };
+      $scope.doRefresh = function() {
+          $http.get('js/data.json').success(function(data) {
+            $scope.artists = data;
+            $scope.$broadcast('scroll.refreshComplete');
+          });
+      }
+    });
+}])
+
+    .controller('CalendarController', ['$scope', '$http', function($scope, $http) {
+        $http.get('js/data.json').success(function(data) {
+            $scope.calendar = data.calendar;
+
+            $scope.onItemDelete = function(dayIndex, item) {
+                $scope.calendar[dayIndex].schedule.splice($scope.calendar[dayIndex].schedule.indexOf(item), 1);
+            };
+            $scope.toggleStar = function(item) {
+                item.star = !item.star;
+            };
+            $scope.doRefresh = function() {
+                $http.get('js/data.json').success(function(data) {
+                    $scope.calendar = data;
+                    $scope.$broadcast('scroll.refreshComplete');
+                });
+            }
+        });
+    }])
